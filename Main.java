3871f8d14f2370482df6709314c5aeb54a395f0f
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-14 13:10:10 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,38 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program tell if all digits of a number are odd
+ * @author Thomas Quigley
  */
 public class Main {
+  // tells if all the digits are odd in a number
+  public static void oddDigits(int userNumber) {
+    // a loop that repeats as long as there is a digit to look at 
+    while (userNumber > 0) {
+      // create the variable that tells if the number is odd or even
+      int oddEven = userNumber % 2;
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+      // if a digit is even then tell the user and end the program, if not do nothing
+      if (oddEven == 0) {
+        System.out.println("Your number has an even digit!");
+        return;
+      }
+
+      // divide by 10 and go onto the next digit
+      userNumber = userNumber / 10;
+    }
+    // all digits are odd so tell user and end
+    System.out.println("Your number has only odd digits in it!");
+    return ;
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // ask for the users number
+    System.out.println("What number would you like to tell if all it's digits are odd?");
+    int userNumber = input.nextInt();
+    
+    // start the oddDigit method
+    oddDigits(userNumber);
     
   }
 }
